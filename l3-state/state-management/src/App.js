fb29922aa888c7b6f34de02858eeac52ebefed18
Nitesh Sharma <nitesh.grep@gmail.com>
2020-05-26 10:12:05 +0530

    Exercise and state management.

---
+++ l3-state/state-management/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListContact from './listcontact';
+
+
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
+
+function App() {
+  return (
+    <ListContact contacts={contacts}/>
+  );
+}
+
+export default App;
